0297346f302dbf1a9a67b80b5e20e022dfca9b19
Andrew <lugovcev@inbox.ru>
2017-01-19 23:35:13 +0300

    first version

--- common.js
+++ common.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-  alert('test');
+  $('button').click(function() {
+    if ($('#input-add').val() != '')
+    $('.list ul').append('<li>' + $('#input-add').val() + ' <span id="del">-</span></li>');
+    $('#input-add').val('');
+  });
+
+  $('body').on('click', '#del', function() {
+    $(this).parent().css('text-decoration', 'line-through');
+  });
 });
