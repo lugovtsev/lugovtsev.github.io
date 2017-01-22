793e42244f446bf876057808ecdc6c7e67bd7688
Andrew <lugovcev@inbox.ru>
2017-01-22 12:28:35 +0300

    created html structure

--- common.js
+++ common.js
@@ -1,11 +1,14 @@
+"use strict";
+
 $(document).ready(function() {
-  $('button').click(function() {
+  /*$('button').click(function() {
     if ($('#input-add').val() != '')
-    $('.list ul').append('<li>' + $('#input-add').val() + ' <span id="del">-</span></li>');
+    $('.list ul').append('<li>' + $('#input-add').val()
+                         + ' <span id="del">-</span></li>');
     $('#input-add').val('');
   });
 
   $('body').on('click', '#del', function() {
     $(this).parent().css('text-decoration', 'line-through');
-  });
+  });*/
 });
